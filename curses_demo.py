c01ee9d748795b857edbb004ee545ab15294fbb5
Sergio Daniel Pires <sergiodanpires@gmail.com>
2023-05-21 21:03:13 -0300

    Cores na send box feita

--- curses_demo.py
+++ curses_demo.py
@@ -1,30 +1,56 @@
 import curses
 import asyncio
+import logging
+from tqdm import tqdm
+from datetime import datetime
+import random
 
 from utils import SendRequestsFront
 
-async def make_request():
-    await asyncio.sleep(3)
-    return
+async def make_request(file: str):
+    time = random.uniform(1, 7)
+    await asyncio.sleep(time)
+    if file[-1] in ("3", "9"):
+        raise Exception("404 Not Found")
+    else:
+        return True
 
-async def safe_request(semaphore: asyncio.Semaphore, file: str):
+async def safe_request(semaphore: asyncio.Semaphore, file: str, front: SendRequestsFront):
     async with(semaphore):
-        return await make_request()
+        index = front.send_box.update(f" ~  {file}\t-")
+        try:
+            result = await make_request(file)
+            front.send_box.update(f" \u2714  {file}\tOK", index=index)
+        except Exception as err:
+            front.logger.error(f"{file} {err}")
+            front.send_box.update(f" x  {file}\t{err}", index=index)
 
 async def make_requests(service, name, front: SendRequestsFront):
-    files = [f"{name}-{x}" for x in range(30)]
-    num_semaphore = 2
+    files = [f"{name}-{x}" for x in range(15)]
+    num_semaphore = 4
     sem = asyncio.Semaphore(num_semaphore)
     tasks = []
     for file in files:
-        tasks.append(asyncio.ensure_future(safe_request(sem, file)))
-        front.send_box.update(file)
+        new_task = asyncio.create_task(safe_request(sem, file, front), name=file)
+        tasks.append(asyncio.ensure_future(new_task))
 
     front.pbar_box.total = len(tasks)
+    c_tqdm = tqdm(total=len(tasks), disable=True)
     for future_task in asyncio.as_completed(tasks):
         front.pbar_box.update(front.pbar_box.finished + 1, len(tasks))
         
         result = await future_task
+        # TQDM Info
+        c_tqdm.update(1)
+        rate = c_tqdm.format_dict["rate"] 
+        rate = rate if rate else 0
+        remaining = (c_tqdm.total - c_tqdm.n) / rate if rate and c_tqdm.total else 0
+        info = [
+            f"Elapsed Time:\t{c_tqdm.format_dict['elapsed']}",
+            f"Remaining:\t{remaining}",
+            f"Avarage:\t{rate}"
+        ]
+        front.info_box.update(info)
 
 async def main():
     front = SendRequestsFront()
@@ -32,14 +58,6 @@ async def main():
     auto_update = asyncio.create_task(front.auto_update())
     try:
         done, pending = await asyncio.wait({auto_update, requests}, return_when=asyncio.FIRST_COMPLETED)
-
-        if done in pending:
-            print("Cabou")
-            print(done)
-            print(pending)
-
-    except Exception as ex:
-       print(str(ex))
     finally:
         front.cleanup()
 
--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import _curses
 from typing import Union
 from math import floor
 from abc import abstractmethod, ABC
+import logging
+import re
 
 class Box(ABC):
     title: str
@@ -12,6 +14,7 @@ class Box(ABC):
     width: int
     height: int
     box: "_curses._CursesWindow"
+    re_patterns: list
 
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
@@ -36,23 +39,75 @@ class StrBox(Box):
         super().__init__(**kwargs)
         self.lines = []
 
-    def update(self, new_lines: Union[list, str]):
+    def update(self, new_lines: Union[list, str], index: int = None):
         if type(new_lines) is list:
             self.lines = new_lines
         else:
-            self.lines.append(new_lines)
+            if index is not None and len(self.lines) > index:
+                self.lines[index] = new_lines
+            else:
+                self.lines.append(new_lines)
 
         self.show()
+        return len(self.lines) - 1
+
+    class TextProcessor:
+        @classmethod
+        def replace_with_index(cls, patterns, replacement, string):
+            combined_pattern = '|'.join(map(re.escape, patterns))
+            result = []
+            cls.last_end = 0
+            cls.replacement_index = 0
+
+            def substitute(match):
+                start = match.start()
+                end = match.end()
+                result.append(string[cls.last_end:start])
+                result.append((string[start:end], replacement[cls.replacement_index]))
+                cls.last_end = end
+                cls.replacement_index = (cls.replacement_index + 1) % len(replacement)
+                return ''
+
+            new_string = re.sub(combined_pattern, substitute, string)
+            result.append(string[cls.last_end:])
+
+            if not result:
+                result.append(new_string)
+
+            return result
+
 
     def show(self):
         list_lines = self.lines
         if len(list_lines) > self.height - 2:
             list_lines = list_lines[len(list_lines)-self.height+2:]
-        try:
-            for idx, line in enumerate(list_lines):
+        
+        self.box.addstr(1, 1, '')
+        for idx, line in enumerate(list_lines):
+            attr = ...
+            c_lines = [line]
+            colored = False
+            for pattern, colors in self.re_patterns:
+                matched = re.match(pattern, line)
+                if matched is not None:
+                    groups = matched.groups()
+                    if len(groups) > 0:
+                        c_lines = self.TextProcessor.replace_with_index(groups, colors, line)
+                        colored = True
+                        break
+
+            # Init line
+            if colored:
+                self.box.addstr(idx+1, 1, '')
+                for line in c_lines:
+                    args = (line, 0)
+                    if type(line) is tuple:
+                        line, attr = line[0], curses.color_pair(line[1])
+                        args = (line, attr)
+                    
+                    self.box.addstr(*args)
+            else:
                 self.box.addstr(idx+1, 1, line)
-        except Exception as ex:
-            raise Exception(f"{str(list_lines)} {self.height} {str(len(list_lines))} {str(len(list_lines)-self.height-2)}")
 
 class PBarBox(Box):
     finished: int
@@ -75,15 +130,18 @@ class PBarBox(Box):
         for height in range(1, self.height - 1):
             self.box.addstr(height, 1, ' ' * progress_w, curses.color_pair(1))
 
-class SendRequestsFront():
+class SendRequestsFront(logging.Handler):
     logs_box: StrBox
     pbar_box: PBarBox
     send_box: StrBox
     stats_box: StrBox
     info_box: StrBox
+    re_patterns: dict
 
     bash_width: int
     bash_height: int
+
+    logger: logging.Logger
 
     def __init__(self):
         self.update = True
@@ -91,22 +149,47 @@ class SendRequestsFront():
         self.initialize()
         self.make_display()
 
+        # Init custom logging
+        logging.Handler.__init__(self)
+        self.formatter = logging.Formatter('%(asctime)-6s %(name)-6s/%(levelname)-6s| %(message)-2s', '%H:%M')
+        logging.addLevelName(logging.INFO, 'info')
+        logging.addLevelName(logging.ERROR, 'error')
+        logging.addLevelName(logging.WARN, 'warn')
+
     def initialize(self):
+        # Start Screen
         self.stdscr = curses.initscr()
         self.bash_height, self.bash_width = self.stdscr.getmaxyx()
         curses.noecho()
         curses.curs_set(0)
         curses.cbreak()
-        curses.start_color()
-        curses.use_default_colors()
-        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
         self.stdscr.keypad(True)
         self.stdscr.clear()
         self.stdscr.refresh()
 
+        # Colors
+        curses.start_color()
+        curses.use_default_colors()
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE) # Loading
+        curses.init_pair(2, curses.COLOR_YELLOW, -1)# WARNING YELLOW
+        curses.init_pair(3, curses.COLOR_RED, -1)   # ERROR RED
+        curses.init_pair(4, curses.COLOR_GREEN, -1) # INFO GREEN
+        curses.init_pair(5, -1, curses.COLOR_BLUE)  # IN QUEUE  SYMBOL
+        curses.init_pair(6, -1, curses.COLOR_GREEN) # OK SYMBOL
+        curses.init_pair(7, -1, curses.COLOR_RED)   # ERROR SYMBOL
+        curses.init_pair(8, 236, -1) # GRAY TEXT
+        self.define_regex_patterns()
+        
+        # Logging
+        self.logger = logging.getLogger('Requests')
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.addHandler(self)
+
     def cleanup(self):
+        self.make_display()
         self.update = False
         if self.stdscr:
+            self.stdscr.getch()
             curses.nocbreak()
             self.stdscr.keypad(False)
             curses.echo()
@@ -117,6 +200,22 @@ class SendRequestsFront():
         while self.update:
             self.make_display()
             await asyncio.sleep(0.1)
+
+    def define_regex_patterns(self) -> dict:
+        # list of: [pattern, [colors]] Colors is for color de matchs
+        re_pattern = {
+            "logs_box": [
+               (r"^.*?\s\w.*\/(warn).*$", [2]),
+               (r"^.*?\s\w.*\/(error).*$", [3]),
+               (r"^.*?\s\w.*\/(info).*$", [4])
+            ],
+            "send_box": [
+                (r"^(\s~\s)", [5]),
+                (re.compile(r'^(\s\u2714\s)(.*$)', re.UNICODE), [6, 8]),
+                (r"^(\sx\s)(.*$)", [7, 8])
+            ]
+        }
+        self.re_patterns = re_pattern
         
     def make_display(self) -> None:
         bash_height, bash_width = self.stdscr.getmaxyx()
@@ -138,15 +237,15 @@ class SendRequestsFront():
         # CRIA CAIXAS
         # Logging
         local = {
-            "logs_box": StrBox(height=log_box_h, width=left_side, x=0, y=0, title="Logs"),
-            "pbar_box": PBarBox(height=pbar_box_h, width=left_side, x=0, y=log_box_h, title="Progresso"),
+            "logs_box": StrBox(height=log_box_h, width=left_side, x=0, y=0, title="Logging", re_patterns=self.re_patterns.get('logs_box', [])),
+            "pbar_box": PBarBox(height=pbar_box_h, width=left_side, x=0, y=log_box_h, title="Progresso", re_patterns=self.re_patterns.get('pbar_box', [])),
 
             # Arquivos
-            "send_box": StrBox(height=files_box_h, width=right_side, x=left_side, y=0, title="Service tasks"),
+            "send_box": StrBox(height=files_box_h, width=right_side, x=left_side, y=0, title="Service tasks", re_patterns=self.re_patterns.get('send_box', [])),
 
             # Subinfo
-            "stats_box": StrBox(height=stats_box_h, width=right_sub, x=right_side+right_sub, y=files_box_h, title="Stats"),
-            "info_box": StrBox(height=info_box_h, width=right_sub, x=right_side+(2*right_sub), y=files_box_h, title="Information")
+            "stats_box": StrBox(height=stats_box_h, width=right_sub, x=right_side+right_sub, y=files_box_h, title="Stats", re_patterns=self.re_patterns.get('stats_box', [])),
+            "info_box": StrBox(height=info_box_h, width=right_sub, x=right_side+(2*right_sub), y=files_box_h, title="Information", re_patterns=self.re_patterns.get('info_box', []))
         }
         for new_object_name in local.keys():
             new_object = local[new_object_name]
@@ -161,3 +260,18 @@ class SendRequestsFront():
             
             self.__setattr__(new_object_name, new_object)
             self.__getattribute__(new_object_name).box.refresh()
+
+    def emit(self, record):
+        try:
+            #msg = self.format(record)
+            msg = self.formatter.format(record)
+            window = self.logs_box
+            fs = "\n%s"
+            try:
+                window.update(msg)
+            except UnicodeError:
+                window.update((fs % msg).encode("UTF-8"))
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
